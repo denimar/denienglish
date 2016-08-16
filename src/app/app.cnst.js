90046dfa3edd4fdb246eed7afe29377af6317096
denimar <denimar@gmail.com>
2016-08-16 13:21:59 -0300

    ...

--- src/app/app.cnst.js
+++ src/app/app.cnst.js
@@ -1,7 +1,7 @@
 //CONSTANTS
 angular.module('app').constant('AppConsts', {
-	//SERVER_URL: 'https://denienglishsrv-denimar.rhcloud.com/', 
-	SERVER_URL: 'http://localhost:8087/denienglish/',
+	SERVER_URL: 'https://denienglishsrv-denimar.rhcloud.com/', 
+	//SERVER_URL: 'http://localhost:8087/denienglish/',
 	//SERVER_URL: 'http://localhost:8084/denienglish/',
 	//SERVER_URL: 'http://localhost:8088/denienglishsrv/',
 });
