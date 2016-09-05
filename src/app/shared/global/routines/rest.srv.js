0938ef306b61de98134d50cdc955fb0248fe4b30
denimar <denimar@gmail.com>
2016-09-05 17:42:45 -0300

    configuring karma+travis+jasmine

---
+++ src/app/shared/global/routines/rest.srv.js
@@ -0,0 +1,52 @@
+'use strict';
+
+angular.module('routinesMdl').service('restSrv', function($q, $http, uiDeniModalSrv) {
+
+	var vm = this;
+
+	var SERVER_URL = 'https://denienglishsrv-denimar.rhcloud.com/'; //Hosted in Open Shift
+	//var SERVER_URL = 'http://localhost:8087/denienglish/'; //Local
+
+    vm.requestWithPromise = function(relativeUrl, parameters, successMessage, confirmMessage) {
+		var deferred = $q.defer();
+
+		var execRequest = function() {
+			var parametrosUrl = {params: parameters};			
+			$http.get(SERVER_URL + relativeUrl, parametrosUrl)
+				.then(function(retornoServer) {
+					if (retornoServer.data.success) {
+						if (successMessage) {						
+							uiDeniModalSrv.ghost(successMessage.title, successMessage.message);
+						}	
+						deferred.resolve(retornoServer);
+					} else {
+						throw retornoServer.data.message;
+					}	
+				})
+				.catch(function(retornoServer) {
+					if (retornoServer.data.message) {
+						uiDeniModalSrv.error(retornoServer.data.message);
+					} else {
+						uiDeniModalSrv.error(retornoServer.data);
+					}
+
+					deferred.reject(retornoServer);
+				});
+		};
+
+		if (confirmMessage) {
+			uiDeniModalSrv.confirm(confirmMessage)
+				.then(function (response) { 
+					if (response.button === 'yes') {
+						execRequest();
+					}
+				});	
+		} else {
+			execRequest();
+		}
+
+		return deferred.promise;
+    };
+
+
+})
